b45a79828b3a2cd16e79cf7d15abfd8053730348
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-27 19:58:18 +0530

    use apollo client directly in getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,52 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import { gql } from "@apollo/client";
 import { Categories, PostCard, PostWidget } from "../components";
-import { getPosts } from "../services";
+import client from "../services/apollo_client";
+import { Post } from "../types";
 
 export async function getStaticProps() {
-	const posts = (await getPosts()) || [];
+	const { data } = await client.query({
+		query: gql`
+			query GetAllPosts {
+				posts {
+					author {
+						name
+						photo {
+							url
+						}
+					}
+					createdAt
+					excerpt
+					featuredImage {
+						url
+					}
+					slug
+					title
+					categories {
+						name
+						slug
+					}
+				}
+			}
+		`
+	});
+
 	return {
 		props: {
-			posts
+			posts: data.posts
 		}
 	};
 }
 
-const Home: NextPage<any> = ({ posts }) => {
+type Props = {
+	posts: [Post];
+};
+
+const Home: React.ComponentType<Props> = ({ posts }) => {
 	return (
 		<div className="container mx-auto mb-8 px-10">
 			<div className="grid grid-cols-1 gap-12 gap-y-12 lg:grid-cols-12">
 				<div className="col-span-1 lg:col-span-8">
-					{posts.map((post: { title: any; excerpt: string }) => (
+					{posts.map((post) => (
 						<PostCard post={post as any} key={post.title} />
 					))}
 				</div>
