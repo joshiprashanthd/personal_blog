974ed0a9cf1d91af7f3b6fc84499a025e37d0ae5
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-27 13:47:34 +0530

    declare env variable in config files

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com"],
+  },
+  env: {
+    GRAPHCMS_ENDPOINT: process.env.GRAPHCMS_ENDPOINT,
+    GRAPHCMS_TOKEN: process.env.GRAPHCMS_TOKEN,
   }
 }
