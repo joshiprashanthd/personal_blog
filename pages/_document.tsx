b2f70b2a31fa40fdcae8f58b7832870d1a60cf5f
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-27 22:20:30 +0530

    add new links to google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,17 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<title>Personal Blog</title>
 				<link rel="icon" href="/favicon.ico" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
-			<body>
+			<body className="bg-gray-100">
 				<Main />
 				<NextScript />
 			</body>
