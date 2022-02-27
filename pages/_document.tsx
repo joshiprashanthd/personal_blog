63b5ba4c2b01ba561fb15dfe3efdfc2bdf89243e
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-27 23:38:34 +0530

    add link ibm plex mono font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,10 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;600;700&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 			<body className="bg-gray-100">
 				<Main />
