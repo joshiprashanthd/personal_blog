9b335ed7f8e594fc41a6156cda8cbee28ece027a
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-28 22:43:29 +0530

    add custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ErrorPage = () => {
+	return (
+		<div className="flex  flex-col items-center justify-center pb-16">
+			<h1 className="mb-4 text-4xl font-bold md:text-5xl">
+				Still in development...
+			</h1>
+			<p>Don't be curious about this...</p>
+		</div>
+	)
+}
+
+export default ErrorPage
