642e1a048559734afe3783ff183b1150109671b5
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-28 22:38:00 +0530

    delete redundant components

--- components/index.js
+++ components/index.js
@@ -1,9 +1,6 @@
-export { default as PostWidget } from "./PostWidget";
-export { default as Categories } from "./Categories";
-export { default as Header } from "./Header";
-export { default as CommentsForm } from "./CommentsForm";
-export { default as Comments } from "./Comments";
-export { default as PostContent } from "./PostContent";
-export { default as Layout } from "./Layout";
-export { default as RecentPostCard } from "./RecentPostCard";
-export { default as Footer} from "./Footer";
+export { default as Header } from './Header'
+export { default as PostContent } from './PostContent'
+export { default as Layout } from './Layout'
+export { default as RecentPostCard } from './RecentPostCard'
+export { default as Footer } from './Footer'
+export { default as SimilarPosts } from './SimilarPosts'
