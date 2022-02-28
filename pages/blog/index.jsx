cc7ceecb4450515cb86943e86e3c6e68d2def475
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-02-28 11:54:21 +0530

    demove redundant enclosing div

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -36,16 +36,14 @@ export async function getStaticProps() {
 
 const Blog = ({ posts }) => {
 	return (
-		<div className="">
-			<div className="grid grid-cols-8 gap-4">
-				<div className="col-span-8 gap-4 px-8 md:col-span-6 md:grid md:grid-cols-2">
-					{posts.map((post) => (
-						<RecentPostCard post={post} />
-					))}
-				</div>
-				<div className="col-span-8 px-12 md:col-span-2">
-					<Categories />
-				</div>
+		<div className="grid grid-cols-8 gap-4">
+			<div className="col-span-8 gap-4 px-8 md:col-span-6 md:grid md:grid-cols-2">
+				{posts.map((post) => (
+					<RecentPostCard post={post} />
+				))}
+			</div>
+			<div className="col-span-8 px-12 md:col-span-2">
+				<Categories />
 			</div>
 		</div>
 	);
