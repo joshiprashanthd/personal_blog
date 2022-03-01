49eb0dc75c95d65b4f02cc9578f37340bf5f1088
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-01 12:24:34 +0530

    add custom title

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,13 +1,20 @@
+import Head from 'next/head'
 import React from 'react'
 
 const ErrorPage = () => {
 	return (
-		<div className="flex  flex-col items-center justify-center pb-16">
-			<h1 className="mb-4 text-4xl font-bold md:text-5xl">
-				Still in development...
-			</h1>
-			<p>Don't be curious about this...</p>
-		</div>
+		<>
+			<Head>
+				<title>404 Not Found</title>
+			</Head>
+
+			<div className="flex  flex-col items-center justify-center pb-16">
+				<h1 className="mb-4 text-4xl font-bold md:text-5xl">
+					Still in development...
+				</h1>
+				<p>Don't be curious about this...</p>
+			</div>
+		</>
 	)
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -38,7 +38,7 @@ const Home = ({ posts }) => {
 	return (
 		<>
 			<Head>
-				<title>Jastor J - Developer</title>
+				<title>Prashant Joshi Blog</title>
 			</Head>
 			<div className="mx-auto flex max-w-3xl flex-col pb-16">
 				<div className="mb-8 flex flex-col-reverse items-center sm:flex-row sm:items-start">
