1b7c445437660c95bb1ba2d87c480445838c6e96
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-02 22:45:12 +0530

    pass href as key to Link to component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,44 +1,44 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React from "react";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
 
 const Header = () => {
-	const router = useRouter();
-	const path = router.pathname;
+	const router = useRouter()
+	const path = router.pathname
 
 	const routes = [
 		{
-			href: "/",
-			name: "Home"
+			href: '/',
+			name: 'Home'
 		},
 		{
-			href: "/projects",
-			name: "Projects"
+			href: '/projects',
+			name: 'Projects'
 		},
 		{
-			href: "/blog",
-			name: "Blog"
+			href: '/blog',
+			name: 'Blog'
 		}
-	];
+	]
 
 	return (
 		<div className="container mx-auto mb-4 flex justify-center">
 			<div className="flex gap-8 py-8">
 				{routes.map((route) => (
-					<Link href={route.href}>
+					<Link href={route.href} key={route.href}>
 						<span
 							className={`cursor-pointer rounded-md px-4 py-2 hover:bg-gray-50 ${
-								path == route.href && "font-semibold text-blue-600"
+								path == route.href && 'font-semibold text-blue-600'
 							}`}
 						>
-							{" "}
-							{route.name}{" "}
+							{' '}
+							{route.name}{' '}
 						</span>
 					</Link>
 				))}
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default Header;
+export default Header
