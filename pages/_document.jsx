331125b8852445fb0ef4f3e9a2f6cc1f79d695d2
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-02 22:44:35 +0530

    update default font to inter

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
@@ -10,11 +10,7 @@ export default function Document() {
 					rel="stylesheet"
 				/>
 				<link
-					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;600;700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
@@ -23,5 +19,5 @@ export default function Document() {
 				<NextScript />
 			</body>
 		</Html>
-	);
+	)
 }
