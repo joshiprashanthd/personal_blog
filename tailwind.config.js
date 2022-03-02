fbc4bdad59e393c616e805939eccd076062fe0a6
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-02 22:32:58 +0530

    remove ibm plex sans from mono fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['IBM Plex Sans', 'sans-serif'],
-        heading: ["Poppins", "sans-serif"],
-        mono: ['IBM Plex Mono', 'monospace']
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['IBM Plex Sans', 'sans-serif'],
+				heading: ['Poppins', 'sans-serif'],
+				mono: ['monospace']
+			}
+		}
+	},
+	plugins: []
 }
