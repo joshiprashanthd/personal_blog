af695f2fdaf473148320ae46ceb283f01bc0adb6
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-03 23:27:53 +0530

    change font sizes for footer content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,30 +20,34 @@ const Footer = () => {
 		<div className="border-t border-gray-200 py-16">
 			<div className="flex flex-col gap-12 sm:flex-row">
 				<div className="flex flex-col gap-4">
-					<span className="block font-semibold text-gray-600">Explore</span>
+					<span className="block text-sm font-semibold text-gray-600">
+						Explore
+					</span>
 					<Link href="/">
-						<p className="cursor-pointer text-lg text-gray-500 hover:text-gray-700">
+						<p className="cursor-pointer text-gray-500 hover:text-gray-700">
 							Home
 						</p>
 					</Link>
 					<Link href="/projects">
-						<p className="cursor-pointer text-lg text-gray-500 hover:text-gray-700">
+						<p className="cursor-pointer  text-gray-500 hover:text-gray-700">
 							Projects
 						</p>
 					</Link>
 					<Link href="/blog">
-						<p className="cursor-pointer text-lg text-gray-500 hover:text-gray-700">
+						<p className="cursor-pointer  text-gray-500 hover:text-gray-700">
 							Blog
 						</p>
 					</Link>
 				</div>
 				<div className="flex flex-col gap-4">
-					<span className="block font-semibold text-gray-600">Categories</span>
+					<span className="block text-sm font-semibold text-gray-600">
+						Categories
+					</span>
 					{data.categories.map((category) => (
 						<Link href={`/blog/category/${category.slug}`}>
 							<p
 								key={category.slug}
-								className="cursor-pointer text-lg text-gray-500 hover:text-gray-700"
+								className="cursor-pointer  text-gray-500 hover:text-gray-700"
 							>
 								{category.name}
 							</p>
@@ -51,14 +55,16 @@ const Footer = () => {
 					))}
 				</div>
 				<div className="flex flex-col gap-4">
-					<span className="block font-semibold text-gray-600">Connect</span>
+					<span className="block text-sm font-semibold text-gray-600">
+						Connect
+					</span>
 					<a href="https://twitter.com/prashantjoshi09" target="_blank">
-						<span className="cursor-pointer text-lg text-gray-500 hover:text-gray-700">
+						<span className="cursor-pointer  text-gray-500 hover:text-gray-700">
 							Twitter
 						</span>
 					</a>
 					<a href="https://github.com/joshiprashanthd" target="_blank">
-						<span className="cursor-pointer text-lg text-gray-500 hover:text-gray-700">
+						<span className="cursor-pointer  text-gray-500 hover:text-gray-700">
 							Github
 						</span>
 					</a>
