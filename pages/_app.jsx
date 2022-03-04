3a0d4072d46403ec62a08a1bdaca777e664bae1f
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-04 19:49:56 +0530

    add progress bar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,29 @@
 import '../styles/globals.css'
 import { Layout } from '../components'
 import { ApolloProvider } from '@apollo/client'
+import NextNProgress from 'nextjs-progressbar'
 import client from '../services/apollo_client'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<ApolloProvider client={client}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ApolloProvider>
+		<>
+			<NextNProgress
+				color="#3b82f6"
+				options={{
+					showSpinner: false,
+					easing: 'ease',
+					speed: 500,
+					minimum: 0.1,
+					trickle: true,
+					trickleSpeed: 200
+				}}
+			/>
+			<ApolloProvider client={client}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ApolloProvider>
+		</>
 	)
 }
 
