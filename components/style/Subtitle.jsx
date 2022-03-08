c03e4283cc6c902832ba86a7813f8bb7f2eb44c6
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-08 13:34:06 +0530

    make subtitle gray

--- components/style/Subtitle.jsx
+++ components/style/Subtitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Subtitle = ({ children, style }) => {
-	return <p className={`font-light md:text-lg ${style}`}>{children}</p>
+	return <p className={`text-gray-500 md:text-lg ${style}`}>{children}</p>
 }
 
 export default Subtitle
