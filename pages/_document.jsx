78ce23a2e567d854874f0214c20a3993c4a7791a
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-08 13:34:57 +0530

    remove redundant font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,10 +6,6 @@ export default function Document() {
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
-					rel="stylesheet"
-				/>
-				<link
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
