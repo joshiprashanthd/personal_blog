05fadc998101c767754548b0fcdaeadd01710e8a
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-08 13:33:57 +0530

    change horixontal padding

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
-			<main className="mx-auto flex max-w-3xl flex-col justify-center px-8 sm:px-4">
+			<main className="mx-auto flex max-w-3xl flex-col justify-center px-8 sm:px-6">
 				<div className="pb-16">{children}</div>
 				<Footer />
 			</main>
