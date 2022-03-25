6c34801ca493fe2e769adcdaf39363a20dcd0c7c
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-25 19:17:54 +0530

    remove tailwind classes from body

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,10 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<link rel="icon" href="/favicon.ico" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
 					rel="stylesheet"
@@ -14,7 +14,7 @@ export default function Document() {
 					rel="stylesheet"
 				/>
 			</Head>
-			<body className="bg-gray-100">
+			<body>
 				<Main />
 				<NextScript />
 			</body>
