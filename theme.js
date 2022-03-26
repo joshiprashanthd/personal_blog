32b303cf7fa2c13441c62a181e5611828b0cab3a
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-03-26 11:55:12 +0530

    separate theme config of chakra ui

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+	fonts: {
+		heading: 'Inter, sans-serif',
+		body: 'Inter, sans-serif',
+		serif: '"IBM Plex Serif", serif',
+		mono: 'monospace'
+	},
+	config: {
+		initialColorMode: 'dark'
+	}
+}
