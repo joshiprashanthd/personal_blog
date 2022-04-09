5fa614e8d1c7746ff2051ba11356353ab7dd4663
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-04-09 20:37:02 +0530

    use chakra component and mdx helpers to fetch posts

--- pages/blog/category/[slug].jsx
+++ pages/blog/category/[slug].jsx
@@ -1,69 +1,27 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { gql } from '@apollo/client'
 import Head from 'next/head'
-import client from '../../../services/apollo_client'
-import { Heading } from '../../../components/style'
+import { Heading, Grid, GridItem, Stack } from '@chakra-ui/react'
 import { PostCard } from '../../../components'
-
-const categoriesPathQuery = gql`
-	query Categories {
-		categories {
-			name
-			slug
-		}
-	}
-`
-
-const postsWithCategoryQuery = gql`
-	query PostsWithCategory($slug: String!) {
-		posts(where: { categories_some: { slug: $slug } }) {
-			id
-			author {
-				name
-				photo {
-					url
-				}
-			}
-			categories {
-				name
-				slug
-			}
-			createdAt
-			excerpt
-			featuredImage {
-				url
-			}
-			slug
-			title
-		}
-	}
-`
+import { getCategories, getPostsByCategory } from '../../../helpers/utilities'
 
 export async function getStaticPaths() {
-	const { data } = await client.query({
-		query: categoriesPathQuery
-	})
+	const categories = getCategories()
 
 	return {
-		paths: data.categories.map((category) => ({
-			params: { slug: category.slug }
+		paths: categories.map((category) => ({
+			params: { slug: category }
 		})),
 		fallback: 'blocking'
 	}
 }
 
 export async function getStaticProps({ params }) {
-	const { data } = await client.query({
-		query: postsWithCategoryQuery,
-		variables: {
-			slug: params.slug
-		}
-	})
+	const posts = await getPostsByCategory(params.slug)
 
 	return {
 		props: {
-			posts: data.posts,
+			posts: posts,
 			category: `${params.slug[0].toUpperCase()}${params.slug.slice(1)}`
 		},
 		revalidate: 60 * 60 * 6
@@ -78,16 +36,18 @@ const Category = ({ posts, category }) => {
 			<Head>
 				<title>Category: {category}</title>
 			</Head>
-			<div>
-				<Heading style="mb-8">Category: {category}</Heading>
-				<div className="grid grid-cols-2 gap-4">
+			<Stack>
+				<Heading mb={8} fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+					Category: {category}
+				</Heading>
+				<Grid templateColumns="repeat(2, 1fr)" gap={4}>
 					{posts.map((post) => (
-						<div className="col-span-2 sm:col-span-1">
-							<PostCard key={post.id} post={post} />
-						</div>
+						<GridItem colSpan={{ base: 2, md: 1 }}>
+							<PostCard key={post.title} post={post.frontmatter} />
+						</GridItem>
 					))}
-				</div>
-			</div>
+				</Grid>
+			</Stack>
 		</>
 	)
 }
