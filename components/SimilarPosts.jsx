343cacade27a7f845ff9a5774b5f33779fc49539
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-04-09 19:54:23 +0530

    use chakra ui components

--- components/SimilarPosts.jsx
+++ components/SimilarPosts.jsx
@@ -1,54 +1,20 @@
-import { gql, useQuery } from '@apollo/client'
 import React from 'react'
-import { RecentPostCard } from '.'
+import { Heading, Stack } from '@chakra-ui/react'
+import { PostCard } from '.'
 
-const query = gql`
-	query GetSimilarPosts($slug: String!, $categories: [String!]) {
-		posts(
-			where: {
-				slug_not: $slug
-				AND: { categories_some: { slug_in: $categories } }
-			}
-			orderBy: createdAt_DESC
-			last: 3
-		) {
-			title
-			excerpt
-			author {
-				name
-				photo {
-					url
-				}
-			}
-			createdAt
-			slug
-		}
-	}
-`
-
-const SimilarPosts = ({ post }) => {
-	const { data, loading, error } = useQuery(query, {
-		variables: {
-			slug: post.slug,
-			categories: post.categories.map((category) => category.slug)
-		}
-	})
-
-	if (loading) return <div>Loading...</div>
-	if (error) return <div>Error!</div>
-
+const SimilarPosts = ({ posts }) => {
 	return (
-		data.posts.length > 0 && (
-			<div className="pb-16">
-				<h2 className="md:text-4x mb-8 text-3xl font-bold">
+		posts.length > 0 && (
+			<Stack>
+				<Heading mb={8} fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
 					You might also like...
-				</h2>
-				<div className="flex flex-col gap-4">
-					{data.posts.map((post) => (
-						<RecentPostCard post={post} />
+				</Heading>
+				<Stack>
+					{posts.map((post) => (
+						<PostCard post={post.frontmatter} key={post.title} />
 					))}
-				</div>
-			</div>
+				</Stack>
+			</Stack>
 		)
 	)
 }
