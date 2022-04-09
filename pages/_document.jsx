3ffa915af934247cfba2eb859757d6e12faba51b
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-04-09 12:04:50 +0530

    remove link to highlight.js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,10 +12,6 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 					rel="stylesheet"
 				/>
-				<link
-					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
-				/>
 			</Head>
 			<body>
 				<Main />
