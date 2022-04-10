f6fc9c3d2502a298f20f28b2c6b55fd40a606db7
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-04-10 11:10:27 +0530

    remove post content component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,4 @@
 export { default as Header } from './Header'
-export { default as PostContent } from './PostContent'
 export { default as Layout } from './Layout'
 export { default as PostCard } from './PostCard'
 export { default as Footer } from './Footer'
