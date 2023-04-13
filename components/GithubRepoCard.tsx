bc464c44bfe89c16006a3761dac9c224ed45d492
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-04-13 20:34:49 +0530

    change repo card description font size to small

--- components/GithubRepoCard.tsx
+++ components/GithubRepoCard.tsx
@@ -10,7 +10,7 @@ const GithubRepoCard: React.FC<{ repo: Repos['repositories']['nodes'][0] }> = ({
 			className="b-hover b-hover-bg block rounded-md px-4 py-2 hover:shadow-md"
 		>
 			<h1 className="mb-1 font-semibold sm:text-lg">{repo.name}</h1>
-			<p className="text-sm font-light sm:text-base">{repo.description}</p>
+			<p className="text-sm font-light">{repo.description}</p>
 		</a>
 	)
 }
