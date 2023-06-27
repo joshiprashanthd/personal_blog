d7f40e10128f9748eb0199efa9f84cdef9240cc2
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 17:52:52 +0530

    remove unused files

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header'
-export { default as Layout } from './Layout'
-export { default as PostCard } from './PostCard'
-export { default as GithubRepoCard } from './GithubRepoCard'
-export { default as MDXComponents } from './MDXComponents'
