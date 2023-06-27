fa769d1c38d36ac8e05810efb592900fcc974f4e
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 15:17:15 +0530

    add projects information in siteConfig

--- helpers/siteConfig.ts
+++ helpers/siteConfig.ts
@@ -14,5 +14,40 @@ export const siteConfig = {
 				href: '/blog'
 			}
 		]
-	}
+	},
+	projects: [
+		{
+			name: 'pygame-projects',
+			description:
+				'Created games like Game Of Life and The Breakout and A-Star Shortest Path Algorithm Simulation.',
+			url: 'https://github.com/joshiprashanthd/pygame-projects',
+			
+		},
+		{
+			name: 'algorithms',
+			description:
+				'Implementation of 16 most used graph algorithms from scratch',
+			url: 'https://github.com/joshiprashanthd/algorithms',
+			
+		},
+		{
+			name: 'neural_network',
+			description:
+				'Implementation of Keras like deep learning library from scratch',
+			url: 'https://github.com/joshiprashanthd/neural_network',
+		},
+		{
+			name: 'flutter-task-management-app',
+			description:
+				'Fully functional Task Management App with local storage and notifications support.',
+			url: 'https://github.com/joshiprashanthd/flutter-task-management-app',
+			
+		},
+		{
+			name: 'gans',
+			description:
+				'Implemented various research papers related to Generative Adversarial Networks using Tensorflow',
+			url: 'https://github.com/joshiprashanthd/gans',
+		}
+	]
 }
