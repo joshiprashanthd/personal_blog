c6df2c03d8b4a47c847d7fb5dc1660bd00159a05
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 15:17:20 +0530

    add types.ts

---
+++ helpers/types.ts
@@ -0,0 +1,16 @@
+export type Project = {
+	name: string
+	description: string
+	url: string
+}
+
+export type User = {
+	avatar_url: string
+	username: string
+	name: string
+	url: string
+	bio: string
+	followers: number
+	following: number
+	location: string
+}
