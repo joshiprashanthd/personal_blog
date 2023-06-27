136f799ca558b00e0d03e06db015ba92a28159c5
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 17:53:46 +0530

    refactor Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,57 +1,16 @@
-import NextLink from 'next/link'
-import { useRouter } from 'next/router'
-import React from 'react'
-import clsx from 'clsx'
+'use client'
+
+import Nav from './Nav'
+import SocialLinks from './SocialLinks'
 
 const Header = () => {
-	const router = useRouter()
-	const path = router.pathname
-
-	const routes = [
-		{
-			href: '/',
-			name: 'home'
-		},
-		{
-			href: '/projects',
-			name: 'projects'
-		},
-		{
-			href: '/blog',
-			name: 'blog'
-		}
-	]
 	return (
-		<nav className="sticky mb-8 mt-8 p-4 font-serif">
-			<div className="flex justify-center space-x-12 ">
-				{routes.map((route) => {
-					const isActive = path === route.href
-					return (
-						<NextLink href={route.href} key={route.href}>
-							<div className="relative h-max w-max">
-								<p
-									className={clsx(
-										'cursor-pointer px-4 py-1 font-serif text-base font-medium tracking-tight sm:text-lg'
-									)}
-								>
-									{route.name}
-								</p>
-								<div
-									className={clsx(
-										'absolute inset-0 -z-10 h-full w-full origin-center rounded-md transition duration-100',
-										{
-											'translate-y-[0px] scale-100 bg-purple-500 opacity-100':
-												isActive,
-											'scale-50 opacity-0': !isActive
-										}
-									)}
-								/>
-							</div>
-						</NextLink>
-					)
-				})}
+		<header className="sticky top-4 flex items-center rounded-lg py-4 backdrop-blur-lg">
+			<div className="flex w-full items-center justify-between">
+				<Nav />
+				<SocialLinks />
 			</div>
-		</nav>
+		</header>
 	)
 }
 
