5abba977236e306afee2fb025107f8865e4268f0
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 17:54:23 +0530

    add BlogPage

---
+++ app/blog/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PostCard from '../../components/PostCard'
+import { allPosts } from 'contentlayer/generated'
+
+const Blog = () => {
+	return (
+		<section>
+			<div className="mb-8 flex items-center">
+				<h1 className="mb-1 pl-4 font-serif text-3xl font-semibold sm:text-4xl">
+					Blog
+				</h1>
+				<div className="mx-4 flex-1 border-t-2 border-gray-500" />
+			</div>
+			<div className="grid grid-cols-1 sm:grid-cols-2">
+				{allPosts.map((post) => (
+					<PostCard key={post.title} post={post} />
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default Blog
