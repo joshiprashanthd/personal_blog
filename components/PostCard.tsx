3594b9fd2db3b4404fda74a7db954b016f809cf2
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 17:54:48 +0530

    refactor PostCard to use contentlayer generated types

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,24 +1,20 @@
-import NextLink from 'next/link'
+import Link from 'next/link'
 import React from 'react'
-import { Post } from '../helpers/utilities'
 import { format } from 'date-fns'
+import { type Post } from 'contentlayer/generated'
 
-const PostCard: React.FC<{ post: Post }> = ({ post }) => {
+const PostCard = ({ post }: { post: Post }) => {
 	return (
-		<NextLink
-			href={`/blog/post/${post.frontmatter.slug}`}
+		<Link
+			href={post.url}
 			className="b-hover b-hover-bg block rounded-md px-4 py-2 hover:shadow-md"
 		>
 			<span className="text-bold font-mono text-xs text-purple-400">
-				{format(new Date(post.frontmatter.publishedAt), 'MMM d, yyyy')}
+				{format(new Date(post.publishedAt), 'MMM d, yyyy')}
 			</span>
-			<h1 className="mb-1 font-semibold sm:text-lg">
-				{post.frontmatter.title}
-			</h1>
-			<p className="text-sm font-light sm:text-sm">
-				{post.frontmatter.summary}
-			</p>
-		</NextLink>
+			<h1 className="mb-1 font-semibold sm:text-lg">{post.title}</h1>
+			<p className="text-sm font-light sm:text-sm">{post.summary}</p>
+		</Link>
 	)
 }
 
