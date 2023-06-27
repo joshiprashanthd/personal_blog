196a2f983e5f3143588b93c9771772b0c2a4274c
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 11:26:57 +0530

    set experimental.appDir to true

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		appDir: true
+	},
 	reactStrictMode: process.env.NODE_ENV !== 'production',
 	env: {
 		GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN
