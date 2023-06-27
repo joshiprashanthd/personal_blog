b89654e1fd2120516555550d6de43fc28328343b
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-27 15:06:20 +0530

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}'
-	],
+	content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				serif: ['Playfair Display'],
-				sans: ['Work Sans'],
-				mono: ['Jetbrains Mono']
-			}
+			fontFamily: {}
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
