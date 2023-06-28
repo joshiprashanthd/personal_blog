3a093e538711b6b626bfbd6b48bb763ed79198af
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-28 18:18:28 +0530

    change styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import SocialLinks from './SocialLinks'
 
 const Header = () => {
 	return (
-		<header className="sticky top-4 flex items-center rounded-lg py-4 backdrop-blur-lg">
+		<header className="sticky top-4 mb-8 flex items-center rounded-lg bg-purple-500/50 px-4 py-4 backdrop-blur-lg">
 			<div className="flex w-full items-center justify-between">
 				<Nav />
 				<SocialLinks />
