2cffc050c3972262408bab0d2116f4691057bb25
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-29 23:13:12 +0530

    add Dropdown Menu for small screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,36 @@
 'use client'
 
+import Link from "next/link"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../components/ui/Dropdown'
 import Nav from './Nav'
 import SocialLinks from './SocialLinks'
+import MenuIcon from '../icons/MenuIcon'
+import { siteConfig } from '../helpers/siteConfig'
 
 const Header = () => {
 	return (
 		<header className="sticky top-0 mb-12 flex items-center rounded-b-lg bg-purple-500/20 px-4 py-4 backdrop-blur-lg">
 			<div className="flex w-full items-center justify-between">
-				<Nav />
+				<div className='hidden sm:flex'>
+					<Nav />
+				</div>
+				<div className='sm:hidden'>
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+								<MenuIcon />
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							{siteConfig.nav.links.map(item => (
+								<DropdownMenuItem asChild key={item.title}>
+									<Link href={item.href}>
+										{item.title}
+									</Link>
+								</DropdownMenuItem>
+							))}
+						</DropdownMenuContent>
+
+					</DropdownMenu>
+				</div>
 				<SocialLinks />
 			</div>
 		</header>
