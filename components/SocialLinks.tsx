81d231ef22d5bba835dda18baff939b4f66b9573
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-29 21:06:00 +0530

    add link to twitter and github accounts

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import TwitterIcon from '../icons/TwitterIcon'
+import GithubIcon from '../icons/GithubIcon'
 
 export default function SocialLinks() {
-	return <div>Social Links</div>
+	return <div className="flex gap-4"> 
+		<a href="https://www.github.com/joshiprashanthd" target='_blank'>
+		<GithubIcon />
+		</a>
+		<a href="https://www.twitter.com/" target="_blank">
+		<TwitterIcon />	
+		</a>
+	</div>
 }
