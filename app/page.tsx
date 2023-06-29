0c33ce3d15d00782c5c3106e4ab62421b41636fb
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-29 23:56:32 +0530

    change home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,54 @@
+import Image from "next/image"
 export default async function HomePage() {
 	return (
-		<section className="flex flex-col items-center justify-center">
-			<img
+		<section className="flex flex-col justify-center">
+			<Image
 				src="https://picsum.photos/200/300?grayscale"
-				className="mb-4 h-40 w-40 rounded-full"
+				className="mb-6 h-40 w-40 rounded-lg"
+				height={160}
+				width={160}
+				alt="Random Image"
 			/>
-			<h1 className="mb-4 block font-serif text-3xl font-semibold sm:text-4xl">
-				Prashant Joshi
-			</h1>
-			<p className="mb-8 text-center text-sm font-light sm:text-base">
-				I am a curious and passionate learner who is constantly exploring the
-				fascinating world of AI. I have a solid background in web development
-				and has completed several successful projects in this field.
-				Additionally, I have a deep understanding of creating and deploying
-				backend servers, which makes me a valuable asset to any tech team. If
-				you're looking for someone who is skilled in AI and web development, I
-				am a perfect fit for you.
-			</p>
-			<div className="flex w-full flex-col items-center justify-center space-y-8 sm:flex-row sm:space-x-8 sm:space-y-0">
-				<a
-					className="b-hover b-hover-bg flex min-w-max items-center rounded-md px-4 py-2 text-sm font-medium text-purple-400 sm:text-base"
-					href="https://twitter.com/jprashanthd"
-				>
-					Connect on twitter
-				</a>
-				<a
-					className="b-hover b-hover-bg flex min-w-max items-center rounded-md px-4 py-2 text-sm font-medium text-purple-400 sm:text-base"
-					href="https://github.com/joshiprashanthd"
-				>
-					Connect on github
-				</a>
+			<div className='flex items-center'>
+				<h1 className="mb-1 font-serif text-2xl font-semibold sm:text-4xl">
+					Prashant Joshi
+				</h1>
+				<div className="mx-4 flex-1 border-t-2 border-purple-500" />
+			</div>
+			<div className='my-8'>
+				<h1 className='text-base sm:text-lg font-medium text-purple-500'>About ME</h1>
+				<p>
+					I am a curious and passionate learner who is constantly exploring the
+					fascinating world of AI. I have a solid background in web development
+					and has completed several successful projects in this field.
+					Additionally, I have a deep understanding of creating and deploying
+					backend servers, which makes me a valuable asset to any tech team. If
+					you're looking for someone who is skilled in AI and web development, I
+					am a perfect fit for you.
+				</p>
+			</div>
+			<div className='grid-cols-1 md:grid-cols-2 grid w-full gap-4'>
+				<div>
+					<h1 className='text-base sm:text-lg font-medium text-purple-500'>Passionate About:</h1>
+					<ul className='ml-6 list-disc'>
+						<li>Deep Learning</li>
+						<li>Natural Language Processing</li>
+						<li>Web Development</li>
+						<li>Cloud Computing</li>
+						<li>Business</li>
+					</ul>
+				</div>
+				<div>
+					<h1 className='text-base sm:text-lg font-medium text-purple-500'>I can code in:</h1>
+					<ul className='ml-6 list-disc'>
+						<li>Python</li>
+						<li>C and C++</li>
+						<li>Java</li>
+						<li>Javascript and Typescript</li>
+						<li>HTML and CSS</li>
+					</ul>
+				</div>
+
 			</div>
 		</section>
 	)
