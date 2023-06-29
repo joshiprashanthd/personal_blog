078263510ba7522ac5915cb2da563d90ebec5488
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-30 00:01:56 +0530

    change text color

--- components/GithubRepoCard.tsx
+++ components/GithubRepoCard.tsx
@@ -7,7 +7,7 @@ const GithubRepoCard = ({ repo }: { repo: Project }) => {
 			href={repo.url}
 			className="group block rounded-md px-4 py-2 transition-all hover:-translate-y-1 hover:bg-purple-500/20"
 		>
-			<h1 className="mb-1 font-semibold group-hover:text-purple-400 sm:text-lg">
+			<h1 className="mb-1 font-medium text-purple-500 sm:text-lg">
 				{repo.name}
 			</h1>
 			<p className="text-sm font-light">{repo.description}</p>
