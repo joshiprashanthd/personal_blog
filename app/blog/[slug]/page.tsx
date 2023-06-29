b47b4b9ff23785b65522c639529aba375cd2b1b9
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-29 23:30:32 +0530

    change seperator color to purple

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -18,11 +18,10 @@ export default async function Post({ params }) {
 		<section>
 			<h1 className="mb-4 font-serif text-4xl font-bold">{post.title}</h1>
 			<div className="mb-8 flex items-center">
-				<div className="rounded-md border border-neutral-800 bg-neutral-900 px-1">
-					<span className="font-mono text-xs sm:text-sm">
-						{format(new Date(post.publishedAt), 'MMM d, yyyy')}
-					</span>
-				</div>
+				<span className="font-mono text-xs sm:text-sm">
+					{format(new Date(post.publishedAt), 'MMM d, yyyy')}
+				</span>
+				<div className="mx-4 flex-1 border-t-2 border-purple-500" />
 			</div>
 			<Mdx code={post.body.code} />
 		</section>
