0a089f464847c4a5b0cf6b289a55c861bb47bbae
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-30 13:12:17 +0530

    add background color to social link button

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -3,12 +3,22 @@ import TwitterIcon from '../icons/TwitterIcon'
 import GithubIcon from '../icons/GithubIcon'
 
 export default function SocialLinks() {
-	return <div className="flex gap-4"> 
-		<a href="https://www.github.com/joshiprashanthd" target='_blank'>
-		<GithubIcon />
-		</a>
-		<a href="https://twitter.com/prashantjoshi09" target="_blank">
-		<TwitterIcon />	
-		</a>
-	</div>
+	return (
+		<div className="flex gap-4">
+			<a
+				href="https://www.github.com/joshiprashanthd"
+				target="_blank"
+				className="rounded-md bg-purple-400/20 p-1.5 shadow-sm"
+			>
+				<GithubIcon />
+			</a>
+			<a
+				href="https://twitter.com/prashantjoshi09"
+				target="_blank"
+				className="rounded-md bg-purple-400/20 p-1.5 shadow-sm"
+			>
+				<TwitterIcon />
+			</a>
+		</div>
+	)
 }
