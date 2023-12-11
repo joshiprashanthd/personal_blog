09973ba13022b840632ab292cf306a8efaf34466
prashant joshi <joshiprashanthd121@gmail.com>
2023-12-11 18:02:12 +0530

    refactor: rename Note with Card

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -3,13 +3,13 @@ import { useMDXComponent } from 'next-contentlayer/hooks'
 
 const components = {
 	Image: (props) => <img alt={props.alt} src={props.src} className="mb-8" />,
-	Note: ({ children, ...props }) => {
+	Card: ({ children, ...props }) => {
 		return (
 			<div
 				{...props}
 				className="rounded-lg border border-neutral-800 bg-neutral-900 p-4 text-white [&>p]:m-0"
 			>
-				<span className="text-sm text-primary">NOTE</span>
+				<span className="text-sm text-primary">{props.title || 'NOTE'}</span>
 				{children}
 			</div>
 		)
