73606673969b51941d4ad790fb2e63146406606d
Prashant Joshi <joshiprashanthd121@gmail.com>
2024-02-26 18:33:03 +0530

    remove unnecessary User type

--- helpers/types.ts
+++ helpers/types.ts
@@ -3,14 +3,3 @@ export type Project = {
 	description: string
 	url: string
 }
-
-export type User = {
-	avatar_url: string
-	username: string
-	name: string
-	url: string
-	bio: string
-	followers: number
-	following: number
-	location: string
-}
